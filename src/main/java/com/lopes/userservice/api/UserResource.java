b68b2fbd98a7b47a4e797d9b37ae214d7b3b743d
Anderson Lopes <anderson.lopes.2010@gmail.com>
2021-07-25 20:26:22 +0200

    Refactoring properties from application.

--- src/main/java/com/lopes/userservice/api/UserResource.java
+++ src/main/java/com/lopes/userservice/api/UserResource.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserResource {
 
-    private UserService userService;
+    private final UserService userService;
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
