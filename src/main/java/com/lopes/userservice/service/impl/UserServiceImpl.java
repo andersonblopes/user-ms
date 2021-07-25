ce344bf9b3d57cf258e37d3de3699f18d38ea3b2
Anderson Lopes <anderson.lopes.2010@gmail.com>
2021-07-25 19:52:05 +0200

    Includes logs.

--- src/main/java/com/lopes/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/lopes/userservice/service/impl/UserServiceImpl.java
@@ -23,28 +23,44 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User saveUser(User user) {
+
+        log.info("Saving new user {} to the database", user.getName());
+
         return userRepository.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+
+        log.info("Saving new role {} to the database", role.getName());
+
         return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToUser(String roleName, String username) {
+
+        log.info("Adding new role {} to user {}", roleName, username);
+
         User user = userRepository.findByUsername(username);
         Role role = roleRepository.findByName(roleName);
+
         user.getRoles().add(role);
     }
 
     @Override
     public User getUser(String username) {
+
+        log.info("Fetching user {}", username);
+
         return userRepository.findByUsername(username);
     }
 
     @Override
     public List<User> getUser() {
+
+        log.info("Fetching all users");
+
         return userRepository.findAll();
     }
 }
