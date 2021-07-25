f36a1487fd97d0d92aff39958ef4746ce179ab92
Anderson Lopes <anderson.lopes.2010@gmail.com>
2021-07-25 19:42:31 +0200

    Fixes role repository.

--- src/main/java/com/lopes/userservice/repository/RoleRepository.java
+++ src/main/java/com/lopes/userservice/repository/RoleRepository.java
@@ -1,10 +1,9 @@
 package com.lopes.userservice.repository;
 
 import com.lopes.userservice.domain.Role;
-import com.lopes.userservice.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    User findByName(String name);
+    Role findByName(String name);
 }
