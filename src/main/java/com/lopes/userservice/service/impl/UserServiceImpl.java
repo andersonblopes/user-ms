abb1d4037ef6daeb1491560fe164d84870e14d51
Anderson Lopes <anderson.lopes.2010@gmail.com>
2021-07-27 00:52:17 +0200

    Encodes password from user.

--- src/main/java/com/lopes/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/lopes/userservice/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +27,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -50,6 +52,8 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     public User saveUser(User user) {
 
         log.info("Saving new user {} to the database", user.getName());
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return userRepository.save(user);
     }
